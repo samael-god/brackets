2ea5d2ca16666e34b2c4543965956319bc5a1cc5
samael-god <nikita.95_@mail.ru>
2023-02-14 09:14:19 +0300

    feat: task finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let mapConfig = {};
+    let stack = [];
+    let openedBrackets = [];
+    bracketsConfig.forEach(item => {
+        mapConfig[item[1]] = item[0];
+        openedBrackets.push(item[0]);
+    })
+    for (let i = 0; i < str.length; i++) {
+        if (openedBrackets.includes(str[i]) && str[i] !== stack.at(-1)) {
+            stack.push(str[i]);
+            continue
+        }
+        if (mapConfig[str[i]] === stack.at(-1)) {
+            stack.pop();
+        } else {
+            stack.push(str[i]);
+        }
+    }
+    return stack.length === 0;
 }
+
+
+
